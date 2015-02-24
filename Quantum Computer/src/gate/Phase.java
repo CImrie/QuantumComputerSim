d6123e729c3faed3ec86caee717ffd3b24c87f6b
MichaelLemmon <michaellemmon@live.co.uk>
2015-02-24 13:26:35 +0000

    Copied Hadamard code over

--- Quantum Computer/src/gate/Phase.java
+++ Quantum Computer/src/gate/Phase.java
@@ -1,13 +1,36 @@
 package gate;
-
-import core.State;
+import core.*;
 
 public class Phase implements Gate{
 
 	@Override
 	public State actOn(State s) {
-		// TODO Auto-generated method stub
+		//ensure the state has only one qubit (no superposition):
+		if (s.get0() > 0 && s.get1() > 0){
+			try {
+				throw new Exception("State given to Hadamard is not a single qubit. Only a single qubit can be accepted");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		//check coefficients to each qubit
+		if (s.get0() > 0){
+			return (new State(1, 0));
+		}
+		else if(s.get1() > 0){
+			return (new State(0,1));
+		}
 		return null;
+	}
+	
+	/**
+	 * This is a test main method to check that hadamard gives appropriate state output.
+	 * @param args
+	 */
+	public static void main(String[] args){
+		Phase p = new Phase();
+		State newS = p.actOn(new State(1, 0));
+		System.out.println(newS.get0() + "|0> " + newS.get1() + "|1> ");
 	}
 
 }
