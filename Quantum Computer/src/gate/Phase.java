960aed73a6f45580e0e1074a5ad6b2be0d98d241
MichaelLemmon <michaellemmon@live.co.uk>
2015-02-24 13:06:57 +0000

    Create Phase

---
+++ Quantum Computer/src/gate/Phase.java
@@ -0,0 +1,13 @@
+package gate;
+
+import core.State;
+
+public class Phase implements Gate{
+
+	@Override
+	public State actOn(State s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
