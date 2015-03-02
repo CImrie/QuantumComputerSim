9220e8999b69a575f3132b6e25d364e4027a22d6
Michael Lemmon <user@server.fake>
2015-03-02 18:09:00 +0000

    Update phase to Qubit

--- Quantum Computer/src/gate/Phase.java
+++ Quantum Computer/src/gate/Phase.java
@@ -6,10 +6,10 @@ public class Phase implements OneQubitGate{
 	@Override
 	public State actOn(Qubit q, double phase) {
 		//check coefficients to each qubit
-		if (q.get0() > 0){
+		if (q.getQubitType() == 0){
 			return (new State(new Complex(1, 0), new Complex(0,0)));
 		}
-		else if(q.get1() > 0){
+		else if(q.getQubitType() == 1){
 			// Use Euler
 			double realPart = Math.cos(phase);
 			double complexPart = Math.sin(phase);
@@ -26,9 +26,8 @@ public class Phase implements OneQubitGate{
 	public static void main(String[] args){
 		Phase p = new Phase();
 		double phaseShift = 45;
-		State newS = p.actOn(new Qubit(0, 1),phaseShift);
-		//State s = new State(new Complex(1/newS.getMagnitude(),0),new Complex(1/newS.getMagnitude(),0));
-		System.out.println((1/newS.getMagnitude()) + "[ " + newS.get0() + "|0> +  " + newS.get1() + "|1> ]");
+		State newS = p.actOn(new Qubit(1),phaseShift);
+		System.out.println(newS.toString());
 	}
 
 	@Override
--- Quantum Computer/src/gate/TwoQubitGate.java
+++ Quantum Computer/src/gate/TwoQubitGate.java
@@ -5,4 +5,6 @@ public interface TwoQubitGate {
 	
 	public State[] actOn(Qubit q1, Qubit q2);
 
+	public State actOn(Qubit q1, Qubit q2, double phase);
+
 }
