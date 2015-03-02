738b5e50d381af629fdecf5abd420758fe16ac93
MichaelLemmon <user@server.fake.fake.fake>
2015-03-02 12:15:03 +0000

    Update to Euler

--- Quantum Computer/src/gate/Phase.java
+++ Quantum Computer/src/gate/Phase.java
@@ -10,10 +10,11 @@ public class Phase implements OneQubitGate{
 			return (new State(new Complex(1, 0), new Complex(0,0)));
 		}
 		else if(q.get1() > 0){
-			Complex i = new Complex(0,1);
-			Complex phaseComplex = new Complex(phase,0);
-			Complex multiplied  = Complex.multiply(i, phaseComplex);
-			return (new State(new Complex(0,0), multiplied));
+			// Use Euler
+			double realPart = Math.cos(phase);
+			double complexPart = Math.sin(phase);
+			Complex toOutput = new Complex(realPart,complexPart);
+			return (new State(new Complex(0,0), toOutput));
 		}
 		return null;
 	}
@@ -24,8 +25,10 @@ public class Phase implements OneQubitGate{
 	 */
 	public static void main(String[] args){
 		Phase p = new Phase();
-		State newS = p.actOn(new Qubit(1, 0));
-		System.out.println(newS.get0() + "|0> " + newS.get1() + "|1> ");
+		double phaseShift = 45;
+		State newS = p.actOn(new Qubit(0, 1),phaseShift);
+		//State s = new State(new Complex(1/newS.getMagnitude(),0),new Complex(1/newS.getMagnitude(),0));
+		System.out.println((1/newS.getMagnitude()) + "[ " + newS.get0() + "|0> +  " + newS.get1() + "|1> ]");
 	}
 
 	@Override
